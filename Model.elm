ae2e8d7845c192a2d39bba5de9392c714ab96399
CarlosIPe <carlos2@compendium.com.ar>
2017-12-01 15:41:14 -0500

    set initial position

--- Model.elm
+++ Model.elm
@@ -31,4 +31,47 @@ type alias Position =
 
 
 initialModel =
-    { pieces = Dict.empty }
+    { pieces = Dict.fromList initialPiecesList }
+
+
+initialPiecesList =
+    [ -- Whites
+      ( ( 0, 0 ), ( Rook, White ) )
+    , ( ( 1, 0 ), ( Knight, White ) )
+    , ( ( 2, 0 ), ( Bishop, White ) )
+    , ( ( 3, 0 ), ( Queen, White ) )
+    , ( ( 4, 0 ), ( King, White ) )
+    , ( ( 5, 0 ), ( Bishop, White ) )
+    , ( ( 6, 0 ), ( Knight, White ) )
+    , ( ( 7, 0 ), ( Rook, White ) )
+
+    -- Pawns
+    , ( ( 0, 1 ), ( Pawn, White ) )
+    , ( ( 1, 1 ), ( Pawn, White ) )
+    , ( ( 2, 1 ), ( Pawn, White ) )
+    , ( ( 3, 1 ), ( Pawn, White ) )
+    , ( ( 4, 1 ), ( Pawn, White ) )
+    , ( ( 5, 1 ), ( Pawn, White ) )
+    , ( ( 6, 1 ), ( Pawn, White ) )
+    , ( ( 7, 1 ), ( Pawn, White ) )
+
+    -- Blacks
+    , ( ( 0, 7 ), ( Rook, Black ) )
+    , ( ( 1, 7 ), ( Knight, Black ) )
+    , ( ( 2, 7 ), ( Bishop, Black ) )
+    , ( ( 3, 7 ), ( Queen, Black ) )
+    , ( ( 4, 7 ), ( King, Black ) )
+    , ( ( 5, 7 ), ( Bishop, Black ) )
+    , ( ( 6, 7 ), ( Knight, Black ) )
+    , ( ( 7, 7 ), ( Rook, Black ) )
+
+    -- Pawns
+    , ( ( 0, 6 ), ( Pawn, White ) )
+    , ( ( 1, 6 ), ( Pawn, White ) )
+    , ( ( 2, 6 ), ( Pawn, White ) )
+    , ( ( 3, 6 ), ( Pawn, White ) )
+    , ( ( 4, 6 ), ( Pawn, White ) )
+    , ( ( 5, 6 ), ( Pawn, White ) )
+    , ( ( 6, 6 ), ( Pawn, White ) )
+    , ( ( 7, 6 ), ( Pawn, White ) )
+    ]
